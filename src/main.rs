45ee16b56c6bbf3340d20439310fda47ce58f293
Curtis Alexander <calex@calex.org>
2020-09-05 14:24:37 -0500

    working after video 7

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ extern crate rocket;
 
 use rocket::response::Redirect;
 
+mod utils;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
@@ -12,10 +14,10 @@ fn index() -> &'static str {
 
 #[get("/search?<cmd>")]
 fn search(cmd: String) -> Redirect {
-    let command = get_command_from_query_string(&cmd);
+    let command = utils::get_command_from_query_string(&cmd);
     let redirect_url = match command {
         "gh" => String::from("https://github.com"),
-        _ => String::from("https://google.com"),
+        _ => utils::google::construct_google_search_from_query(&cmd),
     };
 
     Redirect::to(redirect_url)
@@ -23,33 +25,4 @@ fn search(cmd: String) -> Redirect {
 
 fn main() {
     rocket::ignite().mount("/", routes![index, search]).launch();
-}
-
-fn get_command_from_query_string(query_string: &str) -> &str {
-    if query_string.contains(' ') {
-        let index_of_whitespace = query_string.find(' ').unwrap_or(0);
-        return &query_string[..index_of_whitespace];
-    }
-
-    &query_string
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_get_command_from_query_string_no_whitespace() {
-        // Test with command only
-        let actual = get_command_from_query_string("gh");
-        let expected = "gh";
-        assert_eq!(actual, expected);
-    }
-    #[test]
-    fn test_get_command_from_query_string_with_whitespace() {
-        // Test with command only
-        let actual = get_command_from_query_string("gh @curtisalexander");
-        let expected = "gh";
-        assert_eq!(actual, expected);
-    }
 }
